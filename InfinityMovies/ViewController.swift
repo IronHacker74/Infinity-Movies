bca1c95e61367ef3d3d65e4ba4b644e216fdf655
Andrew <armast97@gmail.com>
2021-12-03 14:39:10 -0800

    adding project to github

---
+++ InfinityMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfinityMovies
+//
+//  Created by Andrew Masters on 12/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
